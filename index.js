1b38073769a6a65b934b7cb5c92f21e3b7f15e1a
Rafael Garnasci <rafael.garnasci@gmail.com>
2022-04-14 19:35:04 -0300

    adding alternative to index.js

--- index.js
+++ index.js
@@ -14,3 +14,31 @@ res.setHeader("Access-Control-Allow-Origin", "*")
 })
 
 app.listen(3000, () => console.log ('Servidor iniciado na porta 3000'));
+
+// const fs = require("fs");
+// const url = require("url");
+// const http = require("http");
+
+// const listCoins = require("./moedas.json");
+
+// const server = http.createServer((req, res) => {
+//   const route = url.parse(req.url);
+
+//   switch (route.pathname) {
+//     case "/contato":
+//       res.end("Pagina contato");
+//       break;
+//     case "/moedas":
+//       res.setHeader("Content-Type", "text/html");
+//       res.write(JSON.stringify(listCoins));
+//       break;
+//     default:
+//       const page = fs.readFileSync("pagina.html");
+//       res.write(page);
+//       break;
+//   }
+
+//   return res.end();
+// });
+
+// server.listen(4000);
